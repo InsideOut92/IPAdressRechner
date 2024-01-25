cdba0a682f291793296e24900f3058b590b02d80
InsideOut92 <146266019+InsideOut92@users.noreply.github.com>
2024-01-25 13:16:24 +0100

    Update IPAddressCalculatorFrame.java

--- IPAddressCalculatorFrame.java
+++ IPAddressCalculatorFrame.java
@@ -4,15 +4,22 @@ public class IPAddressCalculatorFrame extends JFrame{
 
     private IPAddressCalculatorPanel calculatorPanel;
 
+    // Konstruktor für das Hauptfenster
     public IPAddressCalculatorFrame(){
-        setTitle("Patrick's IP Addressen Verunstalter");
+        // Fenstertitel und Größe festlegen
+        setTitle("Patrick's IP Addressen Rechner");
         setSize(400, 200);
+        // Standardverhalten beim Schließen des Fensters setzen
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Instanz der IPAddressCalculatorPanel-Klasse erstellen
         calculatorPanel = new IPAddressCalculatorPanel();
+        // IPAddressCalculatorPanel zum Hauptfenster hinzufügen
         add(calculatorPanel);
 
+        // Das Layout und die Größe des Fensters an die enthaltenen Komponenten anpassen
         pack();
+        // Das Fenster in die Mitte des Bildschirms positionieren
         setLocationRelativeTo(null);
     }
 }
